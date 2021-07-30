6e98e6e51508823d94c196c6a8a82d22ba073353
sastors <87986332+sastors@users.noreply.github.com>
2021-07-30 10:24:09 +0800

    Update jd_cfdtx.js

--- jd_cfdtx.js
+++ jd_cfdtx.js
@@ -3,21 +3,17 @@
 cron 59 11,12,23 * * * jd_cfdtx.js
 更新时间：2021-7-20
 活动入口：京喜APP-我的-京喜财富岛提现
-
 已支持IOS双京东账号,Node.js支持N个京东账号
 脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
 ============Quantumultx===============
 [task_local]
 #京喜财富岛提现
 59 11,12,23 * * * https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_cfdtx.js, tag=京喜财富岛提现, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jxcfd.png, enabled=true
-
 ================Loon==============
 [Script]
 cron "59 11,12,23 * * *" script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_cfdtx.js,tag=京喜财富岛提现
-
 ===============Surge=================
 京喜财富岛提现 = type=cron,cronexp="59 11,12,23 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_cfdtx.js
-
 ============小火箭=========
 京喜财富岛提现 = type=cron,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_cfdtx.js, cronexpr="59 11,12,23 * * *", timeout=3600, enable=true
  */
@@ -106,7 +102,7 @@ async function cfd() {
   try {
     nowTimes = new Date(new Date().getTime() + new Date().getTimezoneOffset() * 60 * 1000 + 8 * 60 * 60 * 1000)
     if ((nowTimes.getHours() === 11 || nowTimes.getHours() === 23) && nowTimes.getMinutes() === 59) {
-      let nowtime = new Date().Format("s.S")
+      let nowtime = new Date().Format("s")
       let starttime = $.isNode() ? (process.env.CFD_STARTTIME ? process.env.CFD_STARTTIME * 1 : 59.9) : ($.getdata('CFD_STARTTIME') ? $.getdata('CFD_STARTTIME') * 1 : 59.9);
       if(nowtime < 59) {
         let sleeptime = (starttime - nowtime) * 1000;
@@ -395,7 +391,11 @@ function taskUrl(function_path, body = '') {
 
 function showMsg() {
   return new Promise(resolve => {
-    message += `提现成功：获得${$.money}元`
+    if ($.money > 0) {
+      message += `提现成功：获得${$.money}元`
+    } else {
+      message += `提现失败：获得空气`
+    }
     if($.money > 0) {
         allMessage += `【京东账号${$.index}】${$.nickName || $.UserName}\n${message}${$.index !== cookiesArr.length ? '\n\n' : '\n\n'}`;
     }
